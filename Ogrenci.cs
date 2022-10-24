c5696a6368599c9de9c4671731a7f4743da70878
yazbikod <aslan_murathan@hotmail.com>
2022-10-25 01:52:29 +0300

    Proje dosyası ekle.

---
+++ Ogrenci.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Öğrenci_Uygulaması
+{
+    public class Ogrenci
+    {
+        private int ogrNo;
+        private string isim;
+        private string soyisim;
+        private int vize1;
+        private int vize2;
+        private int final;
+        private string okulismi;
+
+        public Ogrenci(int _ogrNo, string _isim, string _soyisim, int _vize1, int _vize2, int _final, string _okulismi)
+        {
+            ogrNo = _ogrNo;
+            isim = _isim;
+            soyisim = _soyisim;
+            vize1 = _vize1;
+            vize2 = _vize2;
+            final = _final;
+            okulismi = _okulismi;
+        }
+
+        public void ogrenciBilgileri()
+        {
+            Console.WriteLine("Öğrenci Numarası : " + ogrNo);
+            Console.WriteLine("Öğrenci İsmi : " + isim);
+            Console.WriteLine("Öğrenci Soyismi : " + soyisim);
+            Console.WriteLine("Öğrenci Vize 1 : " + vize1);
+            Console.WriteLine("Öğrenci Vize 2 : " + vize2);
+            Console.WriteLine("Öğrenci Final : " + final);
+            Console.WriteLine("Öğrenci Okul İsmi : " + okulismi);
+        }
+
+        public double ogrenciNotu()
+        {
+            double ortalama = vize1 * 0.2 + vize2 * 0.2 + final * 0.6;
+            return ortalama;
+        }
+
+        public void okulGetir()
+        {
+            Console.WriteLine("Öğrencinin Okulu : " + okulismi);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Öğrenci_Uygulaması;
+
+bool kontrol = true;
+Ogrenci ogrenci1 = new Ogrenci(1, "Murat", "Han", 68, 72, 95, "Salih Alptekin İÖO");
+Console.WriteLine("Uygulamamıza Hoşgeldiniz....");
+
+
+while (kontrol)
+{
+    islemleriGoster();
+
+    string secim = Console.ReadLine();
+
+    switch (secim)
+    {
+        case "1": ogrenci1.ogrenciBilgileri(); break;
+        case "2":
+            double ogrOrtalama = ogrenci1.ogrenciNotu();
+            Console.WriteLine("Öğrenci Ortalaması : " + ogrOrtalama); break;
+        case "3": ogrenci1.okulGetir(); break;
+        case "4": kontrol = false; break;
+    }
+
+
+}
+
+static void islemleriGoster()
+{
+    Console.WriteLine("1 - Öğrenci Bilgilerini Göster");
+    Console.WriteLine("2 - Öğrenci Ortalamasını Göster");
+    Console.WriteLine("3 - Öğrenci Okulunu Göster");
+    Console.WriteLine("4 - Çıkış Yap");
+}
